a83c4183ea4e83317bb405d49679fe7fff31784f
rizkimuliono <rizkimuliono@gmail.com>
2025-11-25 10:16:19 +0700

    update dashboard

--- dashboard.php
+++ dashboard.php
@@ -10,26 +10,89 @@ if (!isset($_SESSION['username'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
     <title>Dashboard</title>
 </head>
 
 <body>
-    <h1>ini halaman dashboard</h1>
-    <h3>Hai, <?= $_SESSION['nama']; ?> <small>(<?= ($_SESSION['type'] == 1) ? "Administrator" : "Member"; ?>)</small></h3>
+    <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
+        <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">My Perpustakaan</a>
+        
+        <ul class="navbar-nav px-3">
+            <li class="nav-item text-nowrap">
+                <a class="nav-link" href="logout.php">Sign out</a>
+            </li>
+        </ul>
+    </nav>
 
-    <ul>
-        <?php if ($_SESSION['type'] == 1) { ?>
-            <li><a href="#">Data User</a></li>
-            <li><a href="#">Data Penulis</a></li>
-            <li><a href="#">Data Buku</a></li>
-            <li><a href="#">Data Peminjaman</a></li>
-        <?php } ?>
-        <?php if ($_SESSION['type'] == 0) { ?>
-            <li><a href="#">Pinjam Buku</a></li>
-        <?php } ?>
+    <div class="container-fluid">
+        <div class="row">
+            <nav class="col-md-2 d-none d-md-block bg-light sidebar">
+                <div class="sidebar-sticky">
+                    <ul class="nav flex-column">
+                        <li class="nav-item">
+                            <a class="nav-link active" href="dashboard.php">
+                                <span data-feather="home"></span>
+                                Dashboard
+                            </a>
+                        </li>
+                        <?php if ($_SESSION['type'] == 1) { ?>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">
+                                    <span data-feather="file"></span>
+                                    Data User
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">
+                                    <span data-feather="shopping-cart"></span>
+                                    Data Penulis
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">
+                                    <span data-feather="shopping-cart"></span>
+                                    Data Buku
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">
+                                    <span data-feather="shopping-cart"></span>
+                                    Data Peminjaman
+                                </a>
+                            </li>
+                        <?php } ?>
+                        <?php if ($_SESSION['type'] == 0) { ?>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">
+                                    <span data-feather="shopping-cart"></span>
+                                    Pinjam Buku
+                                </a>
+                            </li>
+                        <?php } ?>
 
-        <li><a href="logout.php">Logout</a></li>
-    </ul>
+                    </ul>
+
+
+
+                </div>
+            </nav>
+
+            <main role="main" class="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
+                <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
+                    <h1 class="h2">Dashboard</h1>
+                    <div class="btn-toolbar mb-2 mb-md-0">
+                        <div class="btn-group mr-2">
+                            Hai, <?= $_SESSION['nama']; ?><small> (<?= ($_SESSION['type'] == 1) ? "Administrator" : "Member"; ?>)</small>
+                        </div>
+                    </div>
+                </div>
+
+
+            </main>
+        </div>
+    </div>
 </body>
 
 </html>
